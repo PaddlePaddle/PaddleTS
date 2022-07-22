9a71797bc825231a78d3073a156d3c63e64ef99a
Annnnnnnnnnnnn <894577770@qq.com>
2022-07-22 19:57:51 +0800

    Add model framework.

---
+++ paddlets/models/dl/paddlepaddle/callbacks/__init__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+callbacks
+"""
+from paddlets.models.dl.paddlepaddle.callbacks.callbacks import (
+    CallbackContainer,
+    EarlyStopping,
+    Callback,
+    History
+)
+
