1983b1f0cfea1ba574ec81a6211aae4cbdb8f9ed
zhangyanan12 <zhangyanan12@baidu.com>
2022-07-25 14:21:52 +0800

    修改analysis的base文件中加回车换行

--- paddlets/analysis/base.py
+++ paddlets/analysis/base.py
@@ -14,6 +14,7 @@ from paddlets.logger import Logger, raise_if_not, raise_if, raise_log
 logger = Logger(__name__)
 
 
+
 class Analyzer(ABC):
     """
     ``Analyzer`` is the base class for all analyzers.
