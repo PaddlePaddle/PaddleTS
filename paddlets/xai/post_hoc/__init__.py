bab1e0da8acc64e93122bea87d03f46bd0c435a3
lunnwpu <lunnwpu@163.com>
2022-12-14 17:09:50 +0800

    Add xai module. Now support kernel shap.

---
+++ paddlets/xai/post_hoc/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+paddlets xai post_hoc
+"""
+from paddlets.xai.post_hoc.explainer_base import BaseExplainer
+from paddlets.xai.post_hoc.shap_explainer import ShapExplainer
