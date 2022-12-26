0052b28cf06a859dc1be45cad7760adede559412
wangdong2222 <282180453@qq.com>
2022-12-26 13:30:08 +0800

    add anomaly detection mtad_gat model

--- paddlets/models/anomaly/__init__.py
+++ paddlets/models/anomaly/__init__.py
@@ -2,5 +2,11 @@
 # -*- coding:utf-8 -*-
 
 """
-paddlets dl anomaly.
+paddlets anomaly.
 """
+
+from paddlets.models.anomaly.dl.autoencoder import AutoEncoder
+from paddlets.models.anomaly.dl.anomaly_transformer import AnomalyTransformer
+from paddlets.models.anomaly.dl.vae import VAE
+from paddlets.models.anomaly.dl.usad import USAD
+from paddlets.models.anomaly.dl.mtad_gat import MTADGAT
